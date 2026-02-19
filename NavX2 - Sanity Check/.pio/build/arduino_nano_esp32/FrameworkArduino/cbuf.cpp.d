2e74e018449e850ae36ce661bff7cd004d95ed07
Joshua Spiesman <jss265@byu.edu>
2026-02-19 12:28:29 -0700

    Remove .pio build artifacts and ignore them

--- NavX2 - Sanity Check/.pio/build/arduino_nano_esp32/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/arduino_nano_esp32/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/joshu/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/joshu/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- NavX2 - Sanity Check/.pio/build/arduino_nano_esp32/FrameworkArduino/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/arduino_nano_esp32/FrameworkArduino/libb64/cencode.c.o: \
- C:/Users/joshu/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
- C:/Users/joshu/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
